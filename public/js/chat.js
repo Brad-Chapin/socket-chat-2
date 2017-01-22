20eb3b33f4eb6a684abd694035e6c1bf9def6d16
Brad Chapin <paledoor@yahoo.com>
2017-01-22 10:54:28 -0700

    socket chat BnB

---
+++ public/js/chat.js
@@ -0,0 +1,14 @@
+$(function () {
+  let socket = io();
+
+  socket.on("new message", function (message){
+    let $li = $("<li>").text(message);
+    $("#messages").append($li);
+  });
+
+  $("#sendMessage").click(function (e){
+    e.preventDefault();
+    socket.emit("new message", $("#m").val());
+    $("#m").val("");
+  });
+});
